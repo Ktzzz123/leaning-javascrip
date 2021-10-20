fd63f08de8da5870efbdca4d47da386fa0f4aa21
TieuThiKieuTien/18521491 <18521491@gm.uit.edu.vn>
2021-10-20 23:37:32 +0700

    hello

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var course=[
+    {
+        id:055,
+        name:'english',
+        coin:5555
+    },
+    {
+        id:0566,
+        name:'math',
+        coin:0
+    }
+]
+
+var newcourse=course.map(function () {
+    
+})
